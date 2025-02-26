605c63ffbaacd62913253422c8008b096f206c48
j02on <pjylove08@naver.com>
2025-02-26 22:55:27 +0900

    feat : setting
    nx 재세팅 및 ui와 design-token 코드 추가

--- apps/entry-user/src/layout/AppLayout.tsx
+++ apps/entry-user/src/layout/AppLayout.tsx
@@ -1,9 +1,17 @@
+import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 
 export const AppLayout = () => {
   return (
     <>
-      <Outlet />
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
     </>
   );
 };
+
+const MainContainer = styled.main`
+  width: 100vw;
+  margin-top: 64px;
+`;
