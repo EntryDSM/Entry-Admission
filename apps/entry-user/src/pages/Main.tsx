5a99aba2dd2b5f45ab53f0d5480d50324691105a
minsu0707 <mandoo4137@gmail.com>
2025-04-15 11:22:27 +0900

    feat : MOU 기업 배너

--- apps/entry-user/src/pages/Main.tsx
+++ apps/entry-user/src/pages/Main.tsx
@@ -1,37 +1,3 @@
-import { AskNav } from '@entry/ui';
-
-const tabs = [
-  { tabTitle: '전체' },
-  { tabTitle: '입학 문의' },
-  { tabTitle: '진로' },
-  { tabTitle: '학교 생활' },
-  { tabTitle: '기숙사' },
-  { tabTitle: '기타' },
-];
-
-const questions = [
-  {
-    tabTitle: '입학 문의',
-    content: '요즘 학교가 이상해요',
-    answer: '님도 이상해요.',
-  },
-  {
-    tabTitle: '기숙사',
-    content: '라면은 어떻게 먹을 수 있나요?',
-    answer: '건웅쌤 아닌 날에 드세요',
-  },
-  {
-    tabTitle: '진로',
-    content: '개발자는 이제 어떻게 살아남을 수 있나요?',
-    answer: '스티븐 잡스가 되세요.',
-  },
-  {
-    tabTitle: '기타',
-    content: '너무 배고파요...밥 주세요..',
-    answer: '저녁 드세요',
-  },
-];
-
 export const Main = () => {
-  return <AskNav tabs={tabs} questions={questions} isQuestion={true} />;
+  return <div></div>;
 };
--- libs/ui/src/assets/banner/index.ts
+++ libs/ui/src/assets/banner/index.ts
@@ -1,1 +1,3 @@
 export { default as mainBanner } from './mainBanner.svg';
+export * from './VideoBanner';
+export * from './imgs';
