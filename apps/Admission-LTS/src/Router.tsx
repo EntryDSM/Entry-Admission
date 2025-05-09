15e93912335a65b7664438033f6390345813d9f8
j02on <pjylove08@naver.com>
2025-05-09 11:44:12 +0900

    feat (#27) : admission-lts 세팅

---
+++ apps/Admission-LTS/src/Router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { AppLayout } from './layout';
+import { Main } from './pages';
+
+export const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Main />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <div>404</div>,
+  },
+]);
