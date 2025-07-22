4679b889d59bf8a512e84e8a3061a2c382ffed83
minsu0707 <mandoo4137@gmail.com>
2025-07-22 13:55:24 +0900

    fix (#66) :  유저 메인 수정

--- apps/entry-user/src/assets/main/index.ts
+++ apps/entry-user/src/assets/main/index.ts
@@ -3,3 +3,4 @@ export { default as downloadIcon } from './다운로드아이콘.svg';
 export { default as noticeMoveArrowIcon } from './noticeMove.svg';
 export { default as upArrowIcon } from './upArrow.svg';
 export { default as downArrowIcon } from './downArrow.svg';
+export * from './moveRight';
