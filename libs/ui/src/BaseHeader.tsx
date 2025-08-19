878eaa11ce5f84034dbdff8ce6436eb762c3016c
민수 <mandoo4137@gmail.com>
2025-08-19 16:37:38 +0900

    feat (#125) : 어드민 헤더 통계 삭제

--- libs/ui/src/BaseHeader.tsx
+++ libs/ui/src/BaseHeader.tsx
@@ -56,7 +56,6 @@ export const AdminHeader = () => {
     { name: '전형 일정 수정', path: '/admissions-schedule' },
     { name: '지원자 조회', path: '/applicants-list' },
     { name: '공지사항', path: '/notice' },
-    { name: '통계', path: '/' },
     { name: '수식', path: '/formula-calculator' },
   ];
 
