eff9ea92d87e67ca2b6e4018cfa01226ed38207e
j02on <pjylove08@naver.com>
2025-08-29 17:54:49 +0900

    feat (#140) : school search api 추가

---
+++ apps/Admission-LTS/src/apis/school/index.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { instance } from '@entry/util-config';
+
+export const useGetSchoolSearch = (schoolName: string) => {
+  return useQuery({
+    queryKey: ['school', schoolName],
+    queryFn: async () => {
+      const { data } = await instance.get(`/schools`, {
+        params: { school_name: schoolName } 
+      });
+      return data;
+    },
+    enabled: !!schoolName, 
+    staleTime: 5 * 60 * 1000, 
+  });
+};
