8e0adb0343320f4fd0bc31583368cef6abc0c20f
0xC0FFE2 <github1@ncloud.sbs>
2025-10-06 16:43:29 +0900

    chore : 다운로드 페이지 컴포넌트 수정

--- apps/entry-user/src/pages/AdmissionOverviewPage.tsx
+++ apps/entry-user/src/pages/AdmissionOverviewPage.tsx
@@ -17,19 +17,21 @@ export const AdmissionOverviewPage = () => {
             alt="2025학년도 신입생 전형 일정표"
           />
         </TableContainer>
-      <AttachmentSection>
-        <AttachmentTable>
-          <AttachmentRow>
-            <AttachmentTitle>첨부 파일</AttachmentTitle>
-            <AttachmentContent>
-              <AttachmentLink href="https://entrydsm.kr/2026%ED%95%99%EB%85%84%EB%8F%84%20%EB%8C%80%EB%8D%95%EC%86%8C%ED%94%84%ED%8A%B8%EC%9B%A8%EC%96%B4%EB%A7%88%EC%9D%B4%EC%8A%A4%ED%84%B0%EA%B3%A0%EB%93%B1%ED%95%99%EA%B5%90%20%EC%8B%A0%EC%9E%85%EC%83%9D%20%EC%9E%85%ED%95%99%EC%A0%84%ED%98%95%EC%9A%94%EA%B0%95.pdf" download>
-                2025학년도 신입생 전형 요강.pdf
-              </AttachmentLink>
-              <DownloadIcon />
-            </AttachmentContent>
-          </AttachmentRow>
-        </AttachmentTable>
-      </AttachmentSection>
+        <AttachmentSection>
+          <AttachmentTable>
+            <AttachmentRow>
+              <AttachmentTitle>첨부 파일</AttachmentTitle>
+              <AttachmentContent>
+                <AttachmentLink href="https://entrydsm.kr/2026%ED%95%99%EB%85%84%EB%8F%84%20%EB%8C%80%EB%8D%95%EC%86%8C%ED%94%84%ED%8A%B8%EC%9B%A8%EC%96%B4%EB%A7%88%EC%9D%B4%EC%8A%A4%ED%84%B0%EA%B3%A0%EB%93%B1%ED%95%99%EA%B5%90%20%EC%8B%A0%EC%9E%85%EC%83%9D%20%EC%9E%85%ED%95%99%EC%A0%84%ED%98%95%EC%9A%94%EA%B0%95.pdf" download>
+                  2025학년도 신입생 전형 요강.pdf
+                  <IconWrapper>
+                    <DownloadIcon />
+                  </IconWrapper>
+                </AttachmentLink>
+              </AttachmentContent>
+            </AttachmentRow>
+          </AttachmentTable>
+        </AttachmentSection>
       </Flex>
     </PageContainer>
   );
@@ -94,15 +96,22 @@ const AttachmentContent = styled.div`
   display: flex;
   align-items: center;
   padding-left: 20px;
-  gap: 8px;
 `;
 
 const AttachmentLink = styled.a`
   font-size: 14px;
   color: ${colors.gray[500]};
   text-decoration: none;
+  display: flex;
+  align-items: center;
+  gap: 8px;
 
   &:hover {
     text-decoration: underline;
   }
+`;
+
+const IconWrapper = styled.span`
+  display: flex;
+  align-items: center;
 `;
