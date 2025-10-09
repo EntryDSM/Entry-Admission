0d203fccf8c7e89cfe253c2a2ab2d6ebe2f0505b
j02on <pjylove08@naver.com>
2025-10-09 21:08:41 +0900

    fix : 에러처리 이동 수정

--- apps/Admission-LTS/src/apis/admission/index.ts
+++ apps/Admission-LTS/src/apis/admission/index.ts
@@ -24,10 +24,14 @@ export const useAdmissionSubmitPost = <T extends IAdmissionRequest>() => {
         }, 3000)
       } else if (err.response?.status === 500) {
         toast.error("서버 오류가 발생했습니다. 다시 시도해주세요.");
-        navigate('/')
+        setTimeout(() => {
+          window.location.href="https://entrydsm.kr/"
+        }, 3000)
       } else {
         toast.error("원서 제출 중 오류가 발생했습니다.");
-        navigate('/')
+        setTimeout(() => {
+          window.location.href="https://entrydsm.kr/"
+        }, 3000)
       }
     }
   })
