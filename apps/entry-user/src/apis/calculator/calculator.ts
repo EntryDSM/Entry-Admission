e1653e3b26eba156ca1a98415d1fa8c16c2ba777
0xC0FFE2 <github1@ncloud.sbs>
2025-10-10 22:12:10 +0900

    hotfix : Public 인스턴스 사용하게 변경

--- apps/entry-user/src/apis/calculator/calculator.ts
+++ apps/entry-user/src/apis/calculator/calculator.ts
@@ -1,9 +1,9 @@
-import { AdmissionUserInstance } from '@entry/util-config';
+import { AdmissionPublicInstance } from '@entry/util-config';
 import { CalculatorScoreRequest, CalculatorScoreResponse } from './types';
 
 export const calculateScore = async (
   data: CalculatorScoreRequest
 ): Promise<CalculatorScoreResponse> => {
-  const res = await AdmissionUserInstance.post('/api/v1/public/calculator/score', data);
+  const res = await AdmissionPublicInstance.post('/api/v1/public/calculator/score', data);
   return res.data;
 };
