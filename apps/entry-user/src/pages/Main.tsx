2a424f0f4608030811101712911c959708a7d659
민수 <mandoo4137@gmail.com>
2025-10-11 02:52:26 +0900

    fix : 지원하기 로직 수정

--- apps/entry-user/src/pages/Main.tsx
+++ apps/entry-user/src/pages/Main.tsx
@@ -35,21 +35,23 @@ export const Main = () => {
       : false;
 
   const handleApplyClick = () => {
-    if (status?.isSubmitted && isTrueSchedule && isLoggedIn) {
-      window.location.href = 'https://admission.entrydsm.kr';
-    } else if (!isLoggedIn) {
+    if (!isLoggedIn) {
       toast.error('로그인 후 지원이 가능합니다.');
       return;
-    } else if (!isTrueSchedule) {
+    }
+    if (!isTrueSchedule) {
       toast.error('아직 지원 기간이 아닙니다.');
       return;
-    } else if (!status?.isSubmitted) {
-      toast.error('아직 원서를 제출하지 않았습니다.');
+    }
+    if (status?.isSubmitted) {
+      toast.error('이미 원서를 제출하였습니다.');
       return;
     }
+
+    window.location.href = 'https://admission.entrydsm.kr';
   };
 
-  const canApply = isLoggedIn && isTrueSchedule;
+  const canApply = isLoggedIn && isTrueSchedule && status?.isSubmitted;
 
   return (
     <>
