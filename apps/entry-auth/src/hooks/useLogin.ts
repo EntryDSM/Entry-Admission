756716419f84c233b0fdbae74c3f1c521e90bbb3
민수 <mandoo4137@gmail.com>
2025-10-11 09:06:36 +0900

    fix : 로그인 에러 메세지 수정

--- apps/entry-auth/src/hooks/useLogin.ts
+++ apps/entry-auth/src/hooks/useLogin.ts
@@ -16,12 +16,18 @@ export const useUserLogin = () => {
     onError: (error: unknown) => {
       let errorMessage = '로그인에 실패했습니다.';
 
-      // error가 axios error인지 확인
       if (error && typeof error === 'object' && 'response' in error) {
         const axiosError = error as { response: { status: number } };
 
-        if (axiosError.response?.status === 401) {
-          errorMessage = '전화번호 또는 비밀번호가 잘못되었습니다.';
+        switch (axiosError.response?.status) {
+          case 401:
+            errorMessage = '비밀번호가 일치하지 않습니다.';
+            break;
+          case 404:
+            errorMessage = '존재하지 않는 전화번호입니다.';
+            break;
+          default:
+            errorMessage = '로그인에 실패했습니다.';
         }
       }
 
