cf962c602da3e1c74aca95009af2f229779e3a0a
0xC0FFE2 <d@donghyun.cc>
2025-10-14 19:10:42 +0900

    chore : 클라이언트 APM 장착

--- apps/entry-auth/src/main.tsx
+++ apps/entry-auth/src/main.tsx
@@ -1,7 +1,7 @@
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { initializeMeercatEngine } from '@util-config/meercat';
+import { initializeMeercatEngine } from '@entry/util-config';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
