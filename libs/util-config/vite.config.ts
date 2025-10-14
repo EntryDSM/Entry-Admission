8273113c1beb18f7f75455f0a5ae891a405019a7
0xC0FFE2 <d@donghyun.cc>
2025-10-14 18:56:40 +0900

    chore : 클라이언트 APM 장착

--- libs/util-config/vite.config.ts
+++ libs/util-config/vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig(() => ({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime', '@tanstack/react-query'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', '@tanstack/react-query', 'js-cookie'],
     },
   },
 }));
