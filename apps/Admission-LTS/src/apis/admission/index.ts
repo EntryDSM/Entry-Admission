4030905f93d8e821635603517e1513cca04b8e6b
0xC0FFE2 <d@donghyun.cc>
2025-10-20 08:41:39 +0900

    chore : 400도 서버에러 로깅 추가

--- apps/Admission-LTS/src/apis/admission/index.ts
+++ apps/Admission-LTS/src/apis/admission/index.ts
@@ -99,7 +99,7 @@ export const useAdmissionSubmitPost = <T extends IAdmissionRequest>() => {
         setTimeout(() => {
           window.location.href = "https://entrydsm.kr/"
         }, 2000)
-      } else if (err.response?.status === 500 || err.response?.status === 502 || err.response?.status === 503) {
+      } else if (err.response?.status === 400 || err.response?.status === 500 || err.response?.status === 502 || err.response?.status === 503) {
         toast.error("일시적으로 처리할 수 없습니다. 다시 시도해 주세요.")
         await sendSubmissionReport(
           sessionId,
